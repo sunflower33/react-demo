34d67d6791ef6ca686512cd58970904133bbc367
sunflower33 <1451617021@qq.com>
2023-03-22 19:32:21 +0800

    受控组件demo

--- src/demo/受控组件与非受控组件/受控组件.js
+++ src/demo/受控组件与非受控组件/受控组件.js
@@ -1,20 +1,29 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 export default class UncontrolledComponent extends Component {
   state = {
-    username: 'default111'
-  }
+    username: "default111",
+  };
   render() {
     return (
       <div>
         <h1>非受控组件</h1>
-        <input type="text" value={this.state.username}></input>
-        <button onClick={() => console.log(this.myUsername.current.value)}>
-          登录
-        </button>
-        <button onClick={() => (this.myUsername.current.value = "")}>
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={(evt) => {
+            this.setState({ username: evt.target.value });
+          }}
+        ></input>
+        <button onClick={() => console.log(this.state.username)}>登录</button>
+        <button
+          onClick={() => {
+            this.setState({ username: "" });
+          }}
+        >
           重置
         </button>
+        <div>{this.state.username}</div>
       </div>
     );
   }
---
+++ src/demo/受控组件与非受控组件/受控组件demo.js
@@ -0,0 +1,66 @@
+import { Component } from "react";
+
+export default class mapDom extends Component {
+  state = {
+    list: [
+      {
+        id: "test1",
+        text: "test1",
+        checked: false,
+      },
+      {
+        id: "test2",
+        text: "test2",
+        checked: true,
+      },
+      { id: "test3", text: "test3", checked: false },
+    ],
+    keyword: "",
+  };
+
+  render() {
+    let newList = this.state.list.map((item, index) => (
+      <li key={item.id}>
+        <input
+          type="checkbox"
+          checked={item.checked}
+          onChange={() => this.checkHandler(index)}
+        />
+        <span style={{ textDecoration: item.checked ? "line-through" : "" }}>
+          {item.text}
+        </span>
+        <button onClick={this.deleteHandler.bind(this, index)}>Delete</button>
+      </li>
+    ));
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={(evt) => {
+            this.setState({ keyword: evt.target.value });
+          }}
+        />
+        <button onClick={() => this.addItemHandler()}>添加</button>
+        <ul>{newList}</ul>
+      </div>
+    );
+  }
+  checkHandler(index) {
+    const newList = [...this.state.list];
+    newList[index].checked = !newList[index].checked;
+    this.setState({ list: newList });
+  }
+  addItemHandler() {
+    let newList = [...this.state.list];
+    newList.push({
+      id: Math.random() * 1000000,
+      text: this.state.keyword,
+    });
+    this.setState({ list: newList, keyword: "", checked: false });
+  }
+  deleteHandler(index) {
+    let newList = this.state.list;
+    newList.splice(index, 1);
+    this.setState({ list: newList });
+  }
+}
