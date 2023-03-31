e31efdb076624f33d630b045e531c244f65005cf
sunflower33 <1451617021@qq.com>
2023-03-31 23:06:49 +0800

    生命周期

--- src/demo/生命周期/lifecycleDemo.js
+++ src/demo/生命周期/lifecycleDemo.js
@@ -2,9 +2,24 @@ import axios from "axios";
 import BetterScroll from "better-scroll";
 import { Component } from "react";
 
+class Child extends Component {
+  state = {
+    count: 1,
+  };
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      count: nextProps.count,
+    });
+  }
+  render() {
+    return <div>测试componentWillRecieveProps: {this.state.count}</div>;
+  }
+}
+
 export default class BetterScrollComponent extends Component {
   state = {
     list: [],
+    count: 1,
   };
   componentDidMount() {
     axios.get("/test.json").then((res) => {
@@ -16,17 +31,26 @@ export default class BetterScrollComponent extends Component {
     if (JSON.stringify(nextState) === JSON.stringify(this.state)) {
       return false;
     }
+    console.log('shouldComponentUpdate=====')
     return true;
   }
-  
+
   componentDidUpdate(prevProps, prevState) {
     if (prevState.list.length === 0) {
       new BetterScroll(".wrapper");
     }
   }
+
+  getDataHandler = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
   render() {
     return (
       <div>
+        <Child count={this.state.count} />
+        <button onClick={() => this.getDataHandler()}>改变状态</button>
         <div
           className="wrapper"
           style={{ height: "200px", overflow: "hidden", background: "yellow" }}
